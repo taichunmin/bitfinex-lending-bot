bb74b4b00b83e6d778775afe3a2978317d5331b7
taichunmin <taichunmin@gmail.com>
2024-05-30 14:57:03 +0800

    20240530

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # bitfinex-lending-bot
+
+## Related Links
+
+- [放貸機器人介紹](https://evestment.weebly.com/marginbotintro.html)
+- [業界公認！加密貨幣最穩低風險被動收入：我每月躺賺 10 萬利息的秘密！Bitfinex 綠葉放貸教學、收益來源、安全性、新手操作完全說明](https://www.youtube.com/watch?v=OL0cZabjl3U)
+
+## Related GitHub Repos
+
+| Repo | Stars | Forks |
+| ---- | ----- | ----- |
+| [anandanand84/technicalindicators](https://github.com/anandanand84/technicalindicators) | ![](https://img.shields.io/github/stars/anandanand84/technicalindicators?style=flat) | ![](https://img.shields.io/github/forks/anandanand84/technicalindicators?style=flat) |
+| [BitBotFactory/MikaLendingBot](https://github.com/BitBotFactory/MikaLendingBot) | ![](https://img.shields.io/github/stars/BitBotFactory/MikaLendingBot?style=flat) | ![](https://img.shields.io/github/forks/BitBotFactory/MikaLendingBot?style=flat) |
+| [eAndrius/BitfinexLendingBot](https://github.com/eAndrius/BitfinexLendingBot) | ![](https://img.shields.io/github/stars/eAndrius/BitfinexLendingBot?style=flat) | ![](https://img.shields.io/github/forks/eAndrius/BitfinexLendingBot?style=flat) |
+| [huaying/bitfinex-lending-bot](https://github.com/huaying/bitfinex-lending-bot) | ![](https://img.shields.io/github/stars/huaying/bitfinex-lending-bot?style=flat) | ![](https://img.shields.io/github/forks/huaying/bitfinex-lending-bot?style=flat) |
+| [instabot42/funding-bot](https://github.com/instabot42/funding-bot) | ![](https://img.shields.io/github/stars/instabot42/funding-bot?style=flat) | ![](https://img.shields.io/github/forks/instabot42/funding-bot?style=flat) |
+| [drodil/bitfinex_bot](https://github.com/drodil/bitfinex_bot) | ![](https://img.shields.io/github/stars/drodil/bitfinex_bot?style=flat) | ![](https://img.shields.io/github/forks/drodil/bitfinex_bot?style=flat) |
+| [liverpool1026/funding_bot](https://github.com/liverpool1026/funding_bot) | ![](https://img.shields.io/github/stars/liverpool1026/funding_bot?style=flat) | ![](https://img.shields.io/github/forks/liverpool1026/funding_bot?style=flat) |
+| [a6984234/Andy-Bitfinex-Loan-Bot](https://github.com/a6984234/Andy-Bitfinex-Loan-Bot) | ![](https://img.shields.io/github/stars/a6984234/Andy-Bitfinex-Loan-Bot?style=flat) | ![](https://img.shields.io/github/forks/a6984234/Andy-Bitfinex-Loan-Bot?style=flat) |
+
+## Related Lending Bots
+
+- [用 AI 寫 Bitfinex 放貸機器人](https://marco79423.net/articles/%E9%9A%A8%E6%89%8B%E8%A8%98-%E7%94%A8-ai-%E5%AF%AB-bitfinex-%E6%94%BE%E8%B2%B8%E6%A9%9F%E5%99%A8%E4%BA%BA)
+- [BFX 放貸機器人程式碼](https://script.google.com/d/1wutEPT6g9hD7KD5HKloSOa-SW5Yy9hGm5zfTEPgtVZSuxThtCUj49l6h/edit?usp=sharing)
+  - [免費 Bitfinex 放貸機器人](https://yk-study.com/2022/06/29/666-2/)
+  - [程式碼備份 v2.4](https://gist.github.com/taichunmin/0ee4820e3a2cfa9775522baa500dd2d5)
--- bin/funding-auto-renew-1.mjs
+++ bin/funding-auto-renew-1.mjs
@@ -8,16 +8,27 @@ import { z } from 'zod'
 import * as bitfinex from '../lib/bitfinex.mjs'
 import * as telegram from '../lib/telegram.mjs'
 import * as url from 'node:url'
+import JSON5 from 'json5'
 
 const loggers = createLoggersByUrl(import.meta.url)
 const RATE_MIN = 0.0001 // APR 3.65%
 
+const ZodJson5CastObj = zodType => { // parse json5 string to object
+  return z.preprocess(val => {
+    try {
+      return JSON5.parse(val)
+    } catch (err) {
+      return val
+    }
+  }, zodType)
+}
+
 const ZodConfig = z.object({
   amount: z.coerce.number().min(0).default(0),
   currency: z.coerce.string().default('USD'),
-  period: z.coerce.number().int().min(2).max(120).default(2),
   rateMax: z.coerce.number().min(RATE_MIN).default(0.01),
   rateMin: z.coerce.number().min(RATE_MIN).default(0.0002),
+  period: ZodJson5CastObj(z.record(z.coerce.number().int().min(2).max(120), z.number().positive()).default({})),
 })
 
 export async function main () {
@@ -67,10 +78,15 @@ export async function main () {
   })
 
   // 從 FRR 及最近的 N 根 K 棒的 high 中，取前 2 高到前 11 高的資料計算平均值（忽略最高）。
-  const rateTarget = _.chain(candles).map('high').thru(rates => [...rates, fundingStats.frr]).sortBy().slice(-11, -1).sum().thru(rate => rate / 10).clamp(cfg.rateMin, cfg.rateMax).value()
-  loggers.log(_.set({}, 'rateTarget', rateStringify(rateTarget)))
+  const target = { amount: cfg.amount }
+  target.rate = _.chain(candles).map('high').thru(rates => [...rates, fundingStats.frr]).sortBy().slice(-11, -1).sum().thru(rate => rate / 10).clamp(cfg.rateMin, cfg.rateMax).value()
+  target.period = rateToPeriod(cfg.period, target.rate)
+  loggers.log(_.set({}, 'target', {
+    ...target,
+    rate: rateStringify(target.rate),
+  }))
 
-  if (floatIsEqual(rateTarget, autoFunding?.rate ?? 0) && _.isEqual(_.pick(cfg, ['amount', 'period']), _.pick(autoFunding, ['amount', 'period']))) {
+  if (_.isMatchWith(autoFunding, target, floatIsEqual)) {
     loggers.log('Setting of auto-renew no change.')
     return
   }
@@ -78,12 +94,23 @@ export async function main () {
   if (autoFunding) await bitfinex.submitAutoFunding({ ..._.pick(cfg, ['currency']), status: 0 })
   await bitfinex.cancelAllFundingOffers(cfg.currency)
   await bitfinex.submitAutoFunding({
-    ..._.pick(cfg, ['currency', 'amount', 'period']),
-    rate: rateTarget * 100, // percentage of rate
+    ..._.pick(cfg, ['currency', 'amount']),
+    period: target.period,
+    rate: target.rate * 100, // percentage of rate
     status: 1,
-  })
-  await telegram.sendMessage({ text: `funding-auto-renew-1:\nRate of auto-renew changed to ${rateStringify(rateTarget)}` })
-    .catch(err => loggers.error(err))
+  }).catch(err => { throw _.merge(err, { data: { target } }) })
+  await telegram.sendMessage({
+    text: `funding-auto-renew-1:\n以 ${rateStringify(target.rate)} 利率自動借出 ${cfg.currency}，最多 ${target.period} 天`,
+  }).catch(err => loggers.error(err))
+}
+
+export function rateToPeriod (periodMap, rateTarget) {
+  const sortedPeriods = _.chain(periodMap)
+    .map((v, k) => ({ peroid: _.toSafeInteger(k), rate: _.toFinite(v) }))
+    .orderBy(['peroid'], ['desc'])
+    .value()
+  const periodTarget = _.find(sortedPeriods, ({ peroid, rate }) => rateTarget >= rate)?.peroid ?? 2
+  return _.clamp(periodTarget, 2, 120)
 }
 
 class NotMainModuleError extends Error {}
@@ -94,7 +121,7 @@ try {
   await main()
 } catch (err) {
   if (!(err instanceof NotMainModuleError)) {
-    loggers.error(err)
+    loggers.error([err])
     process.exit(1)
   }
 }
